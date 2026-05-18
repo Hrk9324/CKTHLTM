92b5dea9c72e17022b5bafa70247085cc66b0394
GuenNhatKing <guennhatking@gmail.com>
2026-05-18 21:10:19 +0700

    Update db.sql to enforce uniqueness on 'ma_gv' and 'phong_thi' columns

--- server/src/main/resources/db.sql
+++ server/src/main/resources/db.sql
@@ -5,7 +5,7 @@ USE quan_ly_phong_thi;
 -- 1. Tạo bảng cho CanBo
 CREATE TABLE IF NOT EXISTS can_bo (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    ma_gv VARCHAR(20) NOT NULL,
+    ma_gv VARCHAR(20) NOT NULL UNIQUE,
     ho_ten VARCHAR(100) NOT NULL,
     ngay_sinh VARCHAR(20),
     don_vi VARCHAR(100)
@@ -14,14 +14,14 @@ CREATE TABLE IF NOT EXISTS can_bo (
 -- 2. Tạo bảng cho PhongThi
 CREATE TABLE IF NOT EXISTS phong_thi (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    phong_thi VARCHAR(50) NOT NULL,
+    phong_thi VARCHAR(50) NOT NULL UNIQUE,
     dia_diem VARCHAR(100) NOT NULL
 );
 
 -- 3. Tạo bảng cho YeuCau
 CREATE TABLE IF NOT EXISTS yeu_cau (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    so_can_bo INT NOT NULL,
+    so_can_bo INT NOT NULL ,
     so_phong_thi INT NOT NULL,
     line INT NOT NULL
 );
