727654fe37f5dd70ac820f5f7b291c0961497614
GuenNhatKing <guennhatking@gmail.com>
2026-05-21 10:53:18 +0700

    Remove redundant log4j2 status logger settings from App class

--- server/src/main/java/ckthltm/App.java
+++ server/src/main/java/ckthltm/App.java
@@ -4,6 +4,10 @@ import ckthltm.connection.Server;
 
 public class App {
     public static void main(String[] args) {
+        System.setProperty("log4j2.statusLoggerLevel", "OFF");
+        System.setProperty("log4j2.StatusLogger.level", "OFF");
+        System.setProperty("org.apache.logging.log4j.simplelog.StatusLogger.level", "OFF");
+
         Server server = new Server();
         server.start(8124);
     }
